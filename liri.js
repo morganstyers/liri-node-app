de653f872278799db22f50d2794f1a3f3a259af8
Morgan Styers <morganstyers@gmail.com>
2019-09-24 00:04:54 -0400

    "basic structure and checking functionality"

--- liri.js
+++ liri.js
@@ -1,5 +1,40 @@
 require("dotenv").config();
+var fs= require('fs')
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
 
 
+function Commands(userCommand, userInput) {
+    var userCommand = process.argv[2];
+    var userInput = process.argv[3];
+
+    switch (userCommand) {
+        case 'concert-this':
+            findConcert(userInput);
+            break;
+
+        case 'spotify-this-song':
+            listenTo(userInput);
+            break;
+
+        case 'movie-this':
+            watch(userInput)
+            break;
+
+        case 'do-what-it-says':
+            simonSays(userInput)
+            break;
+    }
+}
+function findConcert() {
+    console.log("this will be bands in town")
+}
+function listenTo() {
+    console.log("this will be spotify")
+}
+function watch() {
+    console.log("this will be movie info")
+}
+function simonSays() {
+    console.log("bossy bossy")
+}
+Commands();
