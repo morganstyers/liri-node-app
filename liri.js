c9091042d6c33e79e700fff514f696b78922368a
Morgan Styers <morganstyers@gmail.com>
2019-09-25 17:18:59 -0400

    "working bands in town"

--- liri.js
+++ liri.js
@@ -3,6 +3,13 @@ var fs = require('fs')
 var keys = require("./keys.js");
 var axios = require("axios");
 var moment = require("moment");
+var Spotify = require('node-spotify-api');
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+  };
+  
+
 moment().format();
 var userCommand = process.argv[2];
 var userInput = process.argv[3];
@@ -13,37 +20,44 @@ function Commands(userCommand, userInput) {
 
     switch (userCommand) {
         case 'concert-this':
-                if (!userInput) {
-                    console.log("uh oh! You forgot to tell me who you want to see.")
-                } else{
-            axios
-                .get(
-                    "https://rest.bandsintown.com/artists/" +
-                    userInput +
-                    "/events?app_id=codingbootcamp"
-                )
-                .then(function (response) {
-                    console.log(`-------------------------------
+            if (!userInput) {
+                console.log("uh oh! You forgot to tell me who you want to see.")
+            } else {
+                axios
+                    .get(
+                        "https://rest.bandsintown.com/artists/" +
+                        userInput +
+                        "/events?app_id=codingbootcamp"
+                    )
+                    .then(function (response) {
+                        console.log(`-------------------------------
 Okay, ${userInput} is playing some events:
 `)
 
-                    const concertArray = response.data;
+                        const concertArray = response.data;
 
-                    concertArray.forEach(function (response) {
-                        const formattedDate = moment(response.datetime).format(
-                            "MM/DD/YYYY"
-                        );
-                        console.log(`${response.venue.name}- 
+                        concertArray.forEach(function (response) {
+                            const formattedDate = moment(response.datetime).format(
+                                "MM/DD/YYYY"
+                            );
+                            console.log(`${response.venue.name}- 
 ${response.venue.city}, ${response.venue.region}
 ${formattedDate}
 `);
                         });
                     });
-                }
+            }
             break;
 
         case 'spotify-this-song':
+            if(!userInput){
+                 console.log("sorry, didn't get that. Here's The Sign by Ace Of Base")
+                userInput = "The Sign Ace of Base";
+                spotify.search(
+                    { type: "track", query: `'${userInput}'`, limit: 1 })
+            }else{
             listenTo(userInput);
+            }
             break;
 
         case 'movie-this':
@@ -56,9 +70,14 @@ ${formattedDate}
     }
 }
 
-function listenTo() {
-    console.log("this will be spotify")
-}
+function listenTo(song) {
+    var spotify= new Spotify(keys.spotify)
+    spotify
+  .search({ type: 'track', query: userInput})
+  .then(function(response) {
+    console.log(response);
+  })
+    }
 function watch() {
     console.log("this will be movie info")
 }
