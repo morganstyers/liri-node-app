c6b4541be2b927ec4fd878559fe51b2b378feca4
Morgan Styers <morganstyers@gmail.com>
2019-09-26 00:42:24 -0400

    the rest of the things are starting to work

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log("loaded")
 
-module.exports.Spotify={
+module.exports.spotify={
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
--- liri.js
+++ liri.js
@@ -4,6 +4,7 @@ var keys = require("./keys.js");
 var axios = require("axios");
 var moment = require("moment");
 var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
 
 var userCommand = process.argv[2];
 var term = process.argv.slice(3).join(" ");
@@ -13,38 +14,21 @@ function Commands(userCommand, userInput) {
 
     switch (userCommand) {
         case 'concert-this':
-                if (!userInput) {
-                    console.log("uh oh! You forgot to tell me who you want to see.")
-                } else{
-            axios
-                .get(
-                    "https://rest.bandsintown.com/artists/" +
-                    userInput +
-                    "/events?app_id=codingbootcamp"
-                )
-                .then(function (response) {
-                    console.log(`-------------------------------
-Okay, ${userInput} is playing some events:
-`)
-
-                    const concertArray = response.data;
-
-                    concertArray.forEach(function (response) {
-                        const formattedDate = moment(response.datetime).format(
-                            "MM/DD/YYYY"
-                        );
-                        console.log(`${response.venue.name}- 
-${response.venue.city}, ${response.venue.region}
-${formattedDate}
-`);
-                        });
-                    });
+            if (!userInput) {
+                console.log("uh oh! You forgot to tell me who you want to see.")
+            } 
+             else{
+                 goTo(userInput);
                 }
             break;
 
         case 'spotify-this-song':
-            listenTo(userInput);
+                if(!userInput){
+                    userInput==="The-Sign"
+                }else{
+            listenTo(userInput);}
             break;
+        default:
 
         case 'movie-this':
             watch(userInput)
@@ -56,17 +40,77 @@ ${formattedDate}
     }
 }
 
-function listenTo() {
-    console.log("this will be spotify")
+function goTo(userInput) {
+    axios
+        .get(
+            "https://rest.bandsintown.com/artists/" +
+            userInput +
+            "/events?app_id=codingbootcamp"
+        )
+        .then(function (response) {
+            console.log(`-------------------------------
+Okay, ${userInput} is playing some events:
+`)
+            const concertArray = response.data;
+            concertArray.forEach(function (response) {
+                const formattedDate = moment(response.datetime).format(
+                    "MM/DD/YYYY"
+                );
+                console.log(`${response.venue.name}- 
+${response.venue.city}, ${response.venue.region}
+${formattedDate}
+`);
+            })
+        })
 }
-function watch() {
-    console.log("this will be movie info")
+function listenTo(userInput) {
+    spotify
+    spotify.search({type: 'track' , query: userInput, limit: 5})
+    .then(function(response) {
+    var info=response.tracks.items;
+
+    for (var i = 0; i < info.length; i++) {
+     
+        console.log("artist(s): " + (info[i].artists));
+        console.log("song name: " + info[i].name);
+        console.log("preview song: " + info[i].preview_url);
+        console.log("album: " + info[i].album.name);
+        console.log("-----------------------------------");
+    }})
+    .catch(function(err) {
+      console.error('Error occurred: ' + err); 
+    });
+}
+function watch(userInput) {
+    var URL=("http://www.omdbapi.com/?q="+ userInput +"&y=&plot=short&apikey=trilogy")
+    axios.get(URL)
+.then(function(response){
+var movie=response.data;
+movieData=`
+movie title: ${movie.title}`;
+console.log(movie)
+})
+
 }
 function simonSays() {
     console.log("bossy bossy")
-}
 
+    fs.readFile('random.txt', (err, data) => {
+        if (err) throw err;
+        console.log(data);
+      
 
+        var dataArr = data;
 
+        if (dataArr.length === 2) {
+            options(dataArr[0], dataArr[1]);
+        } else if (dataArr.length === 1) {
+            options(dataArr[0]);
+        }
+    });
+};
+      
+
+      
 
 Commands();
